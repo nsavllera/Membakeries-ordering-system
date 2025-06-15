c4d0c2d0ccca437fd9817c9367642b91d070ebf8
nsavllera <156059313+nsavllera@users.noreply.github.com>
2025-06-15 21:18:03 +0800

    new

--- resources/views/report/inventoryreport/index.blade.php
+++ resources/views/report/inventoryreport/index.blade.php
@@ -1,44 +1,41 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container-fluid">
-	<div class="card">
+    <div class="container-fluid">
+        <div class="card p-4">
 
-		<h3 class="mt-4 mb-0">Inventory Report</h3>
+            <h3 class="mb-3">Inventory Report</h3>
 
-		<div class="card-body bg-white">
+            @if (session("message"))
+                <div class="alert alert-success">
+                    <p>{{ session("message") }}</p>
+                </div>
+            @endif
 
-			@if (session("message"))
-			<div class="alert alert-success mt-3">
-				<p>{{ session("message") }}</p>
-			</div>
-			@endif
-			
-			<form action="{{ route('report.inventoryreport.generateReport') }}" method="GET">
-				<table class="table custom-table">
-				<tr>
-						<td>From Date</td>
-						<td>
-							<input type="datetime-local" name="from_date" value="{{ old('from_date') }}" class="form-control">
-							@error('from_date')
-								<span class="text-danger">{{ $message }}</span>
-							@enderror
-						</td>
-					</tr>
-					<tr>
-						<td>To Date</td>
-						<td>
-							<input type="datetime-local" name="to_date" value="{{ old('to_date') }}" class="form-control">
-							@error('to_date')
-								<span class="text-danger">{{ $message }}</span>
-							@enderror
-						</td>
-					</tr>
-				</table>
-				<button type="submit" class="btn btn-primary btn-sm mt-4">Generate Report</button>
-			</form>
+            <form action="{{ route('report.inventoryreport.generateReport') }}" method="GET">
+                <table class="table custom-table">
+                    <tr>
+                        <td>From Date</td>
+                        <td>
+                            <input type="datetime-local" name="from_date" value="{{ old('from_date') }}" class="form-control">
+                            @error('from_date')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>To Date</td>
+                        <td>
+                            <input type="datetime-local" name="to_date" value="{{ old('to_date') }}" class="form-control">
+                            @error('to_date')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </td>
+                    </tr>
+                </table>
+                <button type="submit" class="btn btn-primary mt-3">Generate Report</button>
+            </form>
 
-	</div>
-</div>
+        </div>
+    </div>
 @endsection
-
