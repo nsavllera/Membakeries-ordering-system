5b4e55e599760835e93c69de759e0d8729c9b339
nsavllera <156059313+nsavllera@users.noreply.github.com>
2025-06-15 23:54:20 +0800

    new

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -56,13 +56,11 @@ class ItemController extends Controller
             'updated_at' => 'required|date',
         ]);
 
-        if ($request->hasFile('image_path') && $request->file('image_path')->isValid()) {
-            try {
-                $uploadedFileUrl = Cloudinary::upload($request->file('image_path')->getRealPath())->getSecurePath();
-                $validated_data['image_path'] = $uploadedFileUrl;
-            } catch (\Exception $e) {
-                return back()->withErrors(['image_path' => 'Image upload failed: ' . $e->getMessage()]);
-            }
+        try {
+            $uploadedFileUrl = Cloudinary::upload($request->file('image_path')->getRealPath())->getSecurePath();
+            $validated_data['image_path'] = $uploadedFileUrl;
+        } catch (\Exception $e) {
+            return back()->withErrors(['image_path' => 'Cloudinary upload failed: ' . $e->getMessage()]);
         }
 
         Item::create($validated_data);
