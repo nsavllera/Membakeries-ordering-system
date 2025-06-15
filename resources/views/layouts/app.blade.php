5f722be6020b53557d310385b5f6c77e6e840e14
nsavllera <156059313+nsavllera@users.noreply.github.com>
2025-06-15 15:07:48 +0800

    new changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,161 @@
             margin-right: 15px;
             color: white;
         }
-    
+        
+
+        /* General Reset */
+        body, h1, p, a, ul, li, button {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Alkalami';
+        }
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f3f4f6;
+        }
+        .header {
+            display: flex;
+            width: 97%;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+            background-color: #493628;
+            border-radius: 8px 8px 0 0;
+                }
+        .header .logo {
+            display: flex;
+            align-items: center;
+        }
+        .header .logo img {
+            height: 70px;
+            margin-right: 10px;
+        }
+        .header .logo .name {
+            color: white;
+            font-size: 1.5rem;
+            font-family: 'Alkalami';
+            margin-top: 6.5%;
+        }
+        .header .actions .nav-link{
+            background-color: #FFF7D1;
+            border-style: outset;
+            padding: 8px 16px;
+            border-radius: 12px;
+            margin-left: 10px;
+            color:  #493628;
+            font-family: 'Alkalami';
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        .header .actions .nav-link:hover {
+            background-color: #e0e7ff;
+        }
+
+        /* Main Container */
+        .container {
+            width: 97%;
+            background-color: rgba(255,255,255,0.6);
+            background-blend-mode: lighten;
+            border-radius: 0 0 8px 8px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            text-align: center;
+        }
+        h1 {
+            margin-top: 10%;
+            font-size: 2rem;
+            margin-bottom: 20px;
+            font-family: 'Alkalami';
+            color: #493628;
+        }
+        p {
+            margin-bottom: 10%;
+            font-size: 1rem;
+            color: #AB886D;
+            line-height: 1.8;
+            font-family: 'Alkalami';
+            padding-left: 10%;
+            padding-right: 10%;
+        }
+        a {
+            color: #493628;
+            text-decoration: none;
+            font-family: 'Alkalami';
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+
+
+        /* Change background color for the navbar */
+        .navbar {
+            background-color: #4a382a;
+            color: white;
+        }
+
+        /* Style buttons */
+        .btn-primary {
+            background-color: #516249;
+            border: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+            font-family: 'Alkalami';
+        }
+
+        .btn-primary:hover {
+            background-color: #b4e2c2;
+        }
+
+        /* Add padding to main content */
+        main {
+            padding: 20px;
+            background-color: #FEF9F2;
+            font-family: 'Alkalami';
+        }
+
+        /* Sidebar styling */
+        .col-md-2 {
+            background-color: #f0f1d6;
+            padding: 15px;
+            border-right: 2px solid #ddd;
+        }
+
+        /*Card Styling*/
+        .card{
+            background-color: #fcdada;
+
+        }
+        /* style.css */
+        .custom-table {
+            border: 1px solid #6c5a5a;
+            border-radius: 15px;
+        }
+
+        .custom-table tr,
+        .custom-table td {
+            background-color: #dde5cf;
+            border-color: #6c5a5a;
+            text-align: left;
+        }
+
+        .jumbotron{
+            border-style: outset;
+            border-color: #e9c2c2;
+            background-color: #FFF7D1;
+            border-radius: 12px;
+
+        }
+
+            .jumbotron .p{
+                margin-left: 20px;;
+                color: #34261c;
+                text-align: left;
+            }
+            
     </style>
 
     <!-- Chart.js (v3 or v4 recommended) -->
